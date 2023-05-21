f38aa01ca8ecfda76b556317d0264b3ce635b90c
Ayoub-MCHIMACH <a.mchimach9690@uca.ac.ma>
2023-05-21 19:27:13 +0100

    second commit

---
+++ app/Http/Resources/AppointmentsWithPatientResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class AppointmentsWithPatientResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'=>(string)$this->id,
+            'attributes'=>[
+                'reason' =>$this->reason,
+                'start_time' => $this->start_time,
+                'end_time' => $this->end_time,
+                'created_at'=>$this->created_at,
+                'updated_at'=>$this->updated_at
+            ],
+            'relationships'=>[
+                'id'=>(string)$this->patient->id,
+                'patient name'=>$this->patient->FullName,
+                'PhoneNumber'=>$this->patient->PhoneNumber,
+                'Adress'=>$this->patient->Adress
+            ],
+        ];
+    }
+}
